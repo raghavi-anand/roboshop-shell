964a89376b5cc3a0f50bcbf33fc395b544bf3191
raghavi-anand <raghavianandkumar@gmail.com>
2024-07-09 08:16:49 +0530

    removing app directory

--- catalogue.sh
+++ catalogue.sh
@@ -13,6 +13,9 @@ dnf install nodejs -y
 echo -e "\e[32m>>>>>> Creating App User<<<<<<<<\e[0m"
 useradd roboshop
 
+echo -e "\e[32m>>>>>> Removing existing App Directory <<<<<<<<\e[0m"
+rm -rf /app
+
 echo -e "\e[32m>>>>>> Creating App Directory <<<<<<<<\e[0m"
 mkdir /app
 
