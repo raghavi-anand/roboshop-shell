608e923918563aa7355cd38c70896daea87621ab
raghavi-anand <raghavianandkumar@gmail.com>
2024-07-09 07:59:52 +0530

    echo commands

--- catalogue.sh
+++ catalogue.sh
@@ -1,20 +1,39 @@
+
+echo ">>>>>> Creating Catalogue Service <<<<<<<<"
 cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo ">>>>>> Creating Mongo Repo <<<<<<<<"
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
+echo ">>>>>> Install NodeJS <<<<<<<<"
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 dnf install nodejs -y
+
+echo ">>>>>> Creating App User<<<<<<<<"
 useradd roboshop
+
+echo ">>>>>> Creating App Directory <<<<<<<<"
 mkdir /app
+
+echo ">>>>>> Download Application Content<<<<<<<<"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+echo ">>>>>> Extract Application content <<<<<<<<"
 cd /app
 unzip /tmp/catalogue.zip
+
+echo ">>>>>>Installing NodeJS dependencies <<<<<<<<"
 cd /app
 npm install
 
+echo ">>>>>> Installing Mongo Client<<<<<<<<"
 dnf install mongodb-org-shell -y
+
+echo ">>>>>> Loading Schema <<<<<<<<"
 mongo --host mongodb.rgdevops159.online </app/schema/catalogue.js
 
+echo ">>>>>> Starting service <<<<<<<<"
 systemctl daemon-reload
 systemctl enable catalogue
 systemctl restart catalogue
--- mysql.sh
+++ mysql.sh
@@ -8,3 +8,4 @@ systemctl enable mysqld
 systemctl start mysqld
 
 mysql_secure_installation --set-root-pass RoboShop@1
+
