103ae557cabc8bcc1b9ff0f3cc4b0fadec600059
raghavi-anand <raghavianandkumar@gmail.com>
2024-07-12 22:13:21 +0530

    rabbitmq input password

--- payment.sh
+++ payment.sh
@@ -1,5 +1,11 @@
 component=payment
-rabbitmq_app_passwd=$1
 source common.sh
+rabbitmq_app_passwd=$1
+
+
+if [ -z "${rabbitmq_app_passwd}" ]; then
+  echo Input password missing
+  exit 1
+fi
 
 func_python
