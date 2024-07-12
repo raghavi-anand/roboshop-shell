92cff9de94c7bbf35b7e7879a5ce99ff83c76c6c
raghavi-anand <raghavianandkumar@gmail.com>
2024-07-12 18:20:09 +0530

    status variable

--- common.sh
+++ common.sh
@@ -3,22 +3,28 @@ log=/tmp/roboshop.log
 func_apppreq(){
   echo -e "\e[32m>>>>>> Creating ${component} Service <<<<<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>$log
+  echo $?
 
   echo -e "\e[32m>>>>>> Creating App User<<<<<<<<\e[0m"
   useradd roboshop &>>$log
+  echo $?
 
   echo -e "\e[32m>>>>>> Removing existing App Directory <<<<<<<<\e[0m"
   rm -rf /app &>>$log
+  echo $?
 
   echo -e "\e[32m>>>>>> Creating App Directory <<<<<<<<\e[0m"
   mkdir /app &>>$log
+  echo $?
 
   echo -e "\e[32m>>>>>> Download Application Content<<<<<<<<\e[0m"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>$log
+  echo $?
 
   echo -e "\e[32m>>>>>> Extract Application content <<<<<<<<\e[0m"
   cd /app &>>$log
   unzip /tmp/${component}.zip &>>$log
+  echo $?
 
 }
 
@@ -27,6 +33,7 @@ func_systemd(){
   systemctl daemon-reload &>>$log
   systemctl enable ${component} &>>$log
   systemctl restart ${component} &>>$log
+  echo $?
 }
 
 func_schema_setup(){
@@ -52,18 +59,19 @@ func_nodejs(){
 
 echo -e "\e[32m>>>>>> Creating Mongo Repo <<<<<<<<\e[0m"
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$log
+echo $?
 
 echo -e "\e[32m>>>>>> Install NodeJS <<<<<<<<\e[0m"
 dnf module disable nodejs -y &>>$log
 dnf module enable nodejs:18 -y &>>$log
 dnf install nodejs -y &>>$log
-
+echo $?
 func_apppreq
 
 echo -e "\e[32m>>>>>>Installing NodeJS dependencies <<<<<<<<\e[0m"
 cd /app &>>$log
 npm install &>>$log
-
+echo $?
 func_schema_setup
 
 func_systemd
