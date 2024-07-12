b2267d90f7a9bd9a5c26d00b3b32e6ef32141c63
raghavi-anand <raghavianandkumar@gmail.com>
2024-07-12 18:33:05 +0530

    func_exit_status

--- common.sh
+++ common.sh
@@ -2,9 +2,9 @@ log=/tmp/roboshop.log
 
 func_exit_status(){
   if [ $? -eq 0]; then
-    echo "\e[32m SUCCESS \e[0m"
+    echo -e "\e[32m SUCCESS \e[0m"
   else
-    echo "\e[31m FAILURE \e[0m"
+    echo -e "\e[31m FAILURE \e[0m"
   fi
 }
 
